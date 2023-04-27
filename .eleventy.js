c64ccf07e470b89b8fe6966b2020e87daa3d01cb
Alicia Wilkerson <matalina@gmail.com>
2023-04-27 14:30:23 -0500

    base page

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,27 @@
-const filters = require('./utils/filters.js')
-const transforms = require('./utils/transforms.js')
-const collections = require('./utils/collections.js')
+const filters = require("./utils/filters.js");
+const transforms = require("./utils/transforms.js");
+const collections = require("./utils/collections.js");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 	// Folders to copy to build dir (See. 1.1)
 	eleventyConfig.addPassthroughCopy("src/static");
 
-	// Filters 
+	// Filters
 	Object.keys(filters).forEach((filterName) => {
-		eleventyConfig.addFilter(filterName, filters[filterName])
-	})
+		eleventyConfig.addFilter(filterName, filters[filterName]);
+	});
 
 	// Transforms
 	Object.keys(transforms).forEach((transformName) => {
-		eleventyConfig.addTransform(transformName, transforms[transformName])
-	})
+		eleventyConfig.addTransform(transformName, transforms[transformName]);
+	});
 
 	// Collections
 	Object.keys(collections).forEach((collectionName) => {
-		eleventyConfig.addCollection(collectionName, collections[collectionName])
-	})
+		eleventyConfig.addCollection(collectionName, collections[collectionName]);
+	});
 
 	// This allows Eleventy to watch for file changes during local development.
 	eleventyConfig.setUseGitIgnore(false);
@@ -29,12 +31,12 @@ module.exports = function (eleventyConfig) {
 			input: "src/",
 			output: "dist",
 			includes: "_includes",
-			layouts: "_layouts"
+			layouts: "_layouts",
 		},
 		templateFormats: ["html", "md", "njk"],
 		htmlTemplateEngine: "njk",
 
 		// 1.1 Enable eleventy to pass dirs specified above
-		passthroughFileCopy: true
+		passthroughFileCopy: true,
 	};
 };
--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,12 @@
 module.exports = {
-	"title": "Eleventy Web Starter",
-	"description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PostCSS and esbuild",
-	"url": "https://eleventywebstarter.netlify.app",
-	"author": "Chris Collins",
-	"meta_data": {
-		"twitter": "@chrissy_dev",
-		"opengraph_default": "/static/opengraph-default.jpg"
+	title: "Ascendant Investigations",
+	description: "Helping those who no one believes",
+	url: "https://ascendant-quest.netlify.app",
+	author: "AJ Hunter",
+	meta_data: {
+		twitter: "@ajs_voices",
+		opengraph_default: "/static/opengraph-default.jpg",
 	},
-	"env": process.env.ELEVENTY_ENV === 'production'
-}
+	env: process.env.ELEVENTY_ENV === "production",
+	year: 2023,
+};
